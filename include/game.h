2910edc6ee1594c4c84b3b65d09e8b926adf2f7b
Santi24Yt <santi24yt@gmail.com>
2024-03-05 10:17:27 -0600

    debug log macro

--- include/game.h
+++ include/game.h
@@ -1,1 +1,25 @@
 #include <curses.h>
+
+#define DEBUG_LEVEL 3
+
+#ifndef DEBUG_LEVEL
+  #define DEBUG_LEVEL 1
+#endif
+
+/* Debug macro */
+#define debug1(fmt, ...)  { \
+  time_t timestamp = time(NULL); \
+  struct tm *tmp; \
+  char times[30]; \
+  tmp = localtime(&timestamp); \
+  strftime(times, sizeof(times), "%x - %H:%M:%S", tmp); \
+  fprintf(stderr, "[%s] %s:%d: " fmt "\n", times, __FILE__, __LINE__, ##__VA_ARGS__); \
+}
+
+#define debug2(fmt, ...) { \
+  if (DEBUG_LEVEL >= 2) { debug1(fmt, ##__VA_ARGS__); } \
+}
+
+#define debug3(fmt, ...) { \
+  if (DEBUG_LEVEL >= 3) { debug1(fmt, ##__VA_ARGS__); } \
+}
