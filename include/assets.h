b36297acdc50ccc60403b7c4405134b2aeaf02f2
Santi24Yt <santi24yt@gmail.com>
2024-03-09 22:57:29 -0600

    titlescreen logo

---
+++ include/assets.h
@@ -0,0 +1,40 @@
+#define logotitle "\
+          # #    NN     NN\n\
+  ###     ###    NNN    NN\n\
+  ####     ###   NN N   NN\n\
+   ####    ###   NN  N  NN\n\
+   ####    ###   NN  N  NN\n\
+    ###    ###   NN  N  NN\n\
+     ##   #####  NN   N NN\n\
+     ####### ##  NN    NNN\n\
+    # #####  ##  NN     NN\n\
+    ####\n\
+    ###  /====\\\n\
+     #  //====/    VVVVV\n\
+       //          VVVVVVV\n\
+       ||         VV     V\n\
+       \\\\         V      V\n\
+        \\\\====\\  VV      VV\n\
+         \\====/  VVVVVVVVV\n\
+                   VVVVV\n\
+   {{{\n\
+   {{{     {{{\n\
+    {{ {{{{{{{{{ %%%%%%%\n\
+    {{{{{{       %%    %%%\n\
+    {{{{         %       %\n\
+    {{{          %%%%  %%\n\
+    {{           %% %%%\n\
+    {{          %%%\n\
+    {{{         %%\n\
+                %%\n\
+  (c) 2024      %%\n\
+By: Santi_24    %%\n\
+gh: @Santi24Yt  %\n\
+dc: @santi_24   %\n\
+               %%\n\
+   \\    /\\     %%\n\
+    )  ( ')    %%\n\
+   (  /  )     %%\n\
+jgs \\(__)|    %%\n\
+"
+
